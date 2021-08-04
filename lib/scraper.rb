2c6bf87e54226f139f8ae618b8e66afd7cc64b8d
Hannah Malm <hannahemalm616@gmail.com>
2021-08-04 08:45:11 -0400

    commit tree d86593fe128693df48191099aa263ca25b225bbc

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,20 @@ require 'pry'
 
 class Scraper
 
+  #responsible for scraping the index page that lists all of the students
+  #uses nokogiri to Open-URI to access the page
+  #return value of this method should be an array of hashes
   def self.scrape_index_page(index_url)
+    #first step is to open the page
+    index_page = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
+    binding.pry
     
+    :name
+    :location
+    :profile_url
   end
 
+  #responsbile for scraping an individual student profile page
   def self.scrape_profile_page(profile_url)
     
   end
